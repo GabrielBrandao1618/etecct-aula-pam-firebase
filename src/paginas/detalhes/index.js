f423d7d6c376bc083d2719fce85537ef1fdf209a
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-11-08 10:56:04 -0300

    Implementada funcionalidade de atualizar alunos

--- src/paginas/detalhes/index.js
+++ src/paginas/detalhes/index.js
@@ -1,4 +1,4 @@
-import {View,Text, Image, StyleSheet, Button} from 'react-native'
+import {View,Text, Image, StyleSheet, Button, TextInput} from 'react-native'
 import React,{useState} from 'react'
 import { useRoute } from '@react-navigation/native';
 
@@ -12,15 +12,28 @@ export default function Detalhes({navigation}){
         ref.remove()
         navigation.goBack()
     }
+    
+    async function handleUpdate(){
+        const ref = firebase.database().ref(`Alunos/${id}`)
+        ref.set({
+            Nome: _nome,
+            Nota1: _nota1,
+            Nota2: _nota2,
+            Nota3: _nota3,
+            Imagem: imagem
+        })
+    }
+
+    const [_nome, setNome] = useState(nome)
+    const [_nota1, setNota1] = useState(nota1)
+    const [_nota2, setNota2] = useState(nota2)
+    const [_nota3, setNota3] = useState(nota3)
 
     return(
         <View style={styles.container}>
             <Text style={styles.title}>
                 {nome}
             </Text>
-            <Text>Nota 1: {nota1}</Text>
-            <Text>Nota 2: {nota2}</Text>
-            <Text>Nota 3: {nota3}</Text>
             <Image 
                 style={{
                     width: 200,
@@ -30,10 +43,37 @@ export default function Detalhes({navigation}){
                     uri: imagem
                 }}
             />
+            <Text>Nome</Text>
+            <TextInput 
+                value={_nome}
+                onChangeText={setNome}
+            />
+
+            <Text>Nota 1</Text>
+            <TextInput 
+                value={_nota1}
+                onChangeText={setNota1}
+            />
+
+            <Text>Nota 2</Text>
+            <TextInput 
+                value={_nota2}
+                onChangeText={setNota2}
+            />
+
+            <Text>Nota 3</Text>
+            <TextInput 
+                value={_nota3}
+                onChangeText={setNota3}
+            />
 
             <Button
                 title="Deletar aluno"
                 onPress={handleDeleteSelf}
+            />
+            <Button
+                title="Atualizar aluno"
+                onPress={handleUpdate}
             />
         </View>
     );
